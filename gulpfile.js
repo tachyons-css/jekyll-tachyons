4e9710db2b8d9ff95279cbb1ffc60ced5f6203f6
mrmrs <hi@mrmrs.cc>
2015-03-16 00:38:48 -0700

    Boom.

---
+++ gulpfile.js
@@ -0,0 +1,60 @@
+// Gulp tasks for JKL Tachyons
+
+// Load plugins
+var gulp = require('gulp'),
+    gutil = require('gulp-util'),
+    basswork = require('gulp-basswork'),
+    watch = require('gulp-watch'),
+    prefix = require('gulp-autoprefixer'),
+    //uncss = require('gulp-uncss'),
+    minifyCSS = require('gulp-minify-css'),
+    //sass = require('gulp-sass'),
+    size = require('gulp-size'),
+    rename = require('gulp-rename'),
+    csslint = require('gulp-csslint'),
+    css = require('css'),
+    browserSync = require('browser-sync'),
+    browserReload = browserSync.reload;
+
+gulp.task('css', function() {
+  gulp.src('./src/jkl-tachyons.css')
+    .pipe(basswork())
+    .pipe(size({gzip: false, showFiles: true, title:'basswork css'}))
+    .pipe(size({gzip: true, showFiles: true, title:'basswork gzipped css'}))
+    .pipe(gulp.dest('./css'))
+    .pipe(minifyCSS())
+    .pipe(rename({ extname: '.min.css' }))
+    .pipe(size({gzip: false, showFiles: true, title:'basswork minified'}))
+    .pipe(size({gzip: true, showFiles: true, title:'basswork minified'}))
+    .pipe(gulp.dest('./css'));
+});
+
+// Initialize browser-sync which starts a static server also allows for
+// browsers to reload on filesave
+gulp.task('browser-sync', function() {
+    browserSync.init(null, {
+        server: {
+            baseDir: "./_site/"
+        }
+    });
+});
+
+// Function to call for reloading browsers
+gulp.task('bs-reload', function () {
+    browserSync.reload();
+});
+
+/*
+   DEFAULT TASK
+
+ • Process sass then auto-prefixes and lints outputted css
+ • Starts a server on port 3000
+ • Reloads browsers when you change html or sass files
+
+*/
+gulp.task('default', ['css', 'bs-reload', 'browser-sync'], function(){
+  gulp.start(['css', 'bs-reload']);
+  gulp.watch('src/*', ['css']);
+  gulp.watch(['*.html', './**/*.html'], ['bs-reload']);
+});
+
